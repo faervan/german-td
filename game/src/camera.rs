be8e272d93c10e982d3b9b065cef7ebc281d4978
KirmesBude <kirmesbude@gmail.com>
2026-02-23 23:01:52 +0100

    Kinda janky camera grab

--- game/src/camera.rs
+++ game/src/camera.rs
@@ -7,7 +7,10 @@ use crate::prelude::*;
 
 pub(crate) fn plugin(app: &mut App) {
     app.add_systems(Startup, (spawn, cursor_grab))
-        .add_systems(Update, (move_camera_key, move_camera_edge))
+        .add_systems(
+            Update,
+            (move_camera_key, move_camera_edge, move_camera_grab),
+        )
         .add_systems(
             Update,
             toggle_cursor.run_if(input_just_pressed(KeyCode::Escape)),
@@ -40,7 +43,7 @@ const RIGHT_KEYS: [KeyCode; 2] = [KeyCode::KeyD, KeyCode::ArrowRight];
 const VELOCITY: f32 = 50.0;
 
 // Key based camera movement
-// TODO: Replace with physics based movement and unify with edge panning?
+// TODO: Replace with physics based movement and unify with other movement?
 fn move_camera_key(
     time: Res<Time>,
     keys: Res<ButtonInput<KeyCode>>,
@@ -70,8 +73,10 @@ fn move_camera_key(
 // edge pan tolerance in percent
 const EDGE_PAN_TOLERANCE: f32 = 0.05;
 
+// Mouse edge pan based movement
 // Cursor is considered outside to the very right, so we operate on the cached cursor position
 // Only runs if cursor mode is confined
+// TODO: Change CursorIcon
 fn move_camera_edge(
     time: Res<Time>,
     primary_window: Single<&Window, With<PrimaryWindow>>,
@@ -107,6 +112,49 @@ fn move_camera_edge(
         }
 
         let direction = Vec3::new(x, 0.0, z).normalize();
+
+        if direction.length() > 0.0 {
+            transform.translation += direction * VELOCITY * time.delta_secs();
+        }
+    }
+}
+
+// Mouse grab based movement
+// Middle mouse button down saved current cursor position
+// current cursor position compared to saved cursor position determines direction (reversed?)
+// Middle button up resets saved current cursor position
+// TODO: Change CursorIcon
+// TODO: Visualize grab_cursor_position
+fn move_camera_grab(
+    time: Res<Time>,
+    mouse: Res<ButtonInput<MouseButton>>,
+    primary_window: Single<&Window, With<PrimaryWindow>>,
+    cursor_options: Single<&CursorOptions>,
+    mut transform: Single<&mut Transform, With<PrimaryCamera>>,
+    mut cached_cursor_position: Local<Option<Vec2>>,
+    mut grab_cursor_position: Local<Option<Vec2>>,
+) {
+    if !matches!(cursor_options.grab_mode, CursorGrabMode::Confined) {
+        return;
+    }
+
+    if let Some(cursor_position) = primary_window.cursor_position() {
+        *cached_cursor_position = Some(cursor_position);
+    }
+
+    if mouse.just_released(MouseButton::Middle) {
+        *grab_cursor_position = None;
+    }
+    if mouse.just_pressed(MouseButton::Middle) {
+        *grab_cursor_position = *cached_cursor_position;
+    }
+
+    if let Some(grab_cursor_position) = *grab_cursor_position
+        && let Some(cursor_position) = *cached_cursor_position
+    {
+        let delta = grab_cursor_position - cursor_position;
+
+        let direction = Vec3::new(delta.x, 0.0, delta.y).normalize();
 
         if direction.length() > 0.0 {
             transform.translation += direction * VELOCITY * time.delta_secs();
