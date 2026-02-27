47f1cdc9df128a41eeb8347376bae8564303a9ed
faervan <faervan@proton.me>
2026-02-27 01:03:59 +0100

    fix: map shadow artifacts
    by setting NotShadowCaster on it

--- core/src/skein_spawners.rs
+++ core/src/skein_spawners.rs
@@ -1,3 +1,5 @@
+use bevy::light::NotShadowCaster;
+
 use crate::prelude::*;
 
 pub(super) fn plugin(app: &mut App) {
@@ -16,14 +18,31 @@ struct InsertLight {
 impl InsertLight {
     fn on_add(mut world: DeferredWorld, hook: HookContext) {
         let light = world.get::<InsertLight>(hook.entity).unwrap().clone();
-        dbg!("spawning", &light);
 
-        world.commands().entity(hook.entity).insert(PointLight {
-            intensity: light.intensity,
-            color: light.color,
-            range: light.range,
-            shadows_enabled: true,
-            ..Default::default()
-        });
+        world.commands().entity(hook.entity).insert((
+            PointLight {
+                intensity: light.intensity,
+                color: light.color,
+                range: light.range,
+                shadows_enabled: true,
+                ..Default::default()
+            },
+            DelayedChildrenDespawn,
+        ));
+    }
+}
+
+#[derive(Component, Reflect, Default, Debug, Clone)]
+#[reflect(Component, Default)]
+#[component(on_add)]
+struct SetChildNonShadowCaster;
+
+impl SetChildNonShadowCaster {
+    fn on_add(mut world: DeferredWorld, hook: HookContext) {
+        let children = world.get::<Children>(hook.entity).unwrap().to_vec();
+
+        for child in children {
+            world.commands().entity(child).insert(NotShadowCaster);
+        }
     }
 }
