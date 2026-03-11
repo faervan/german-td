b1dd8dff580e1a8cfe99b9e382d6b6ceece18c9b
KirmesBude <kirmesbude@gmail.com>
2026-03-11 20:05:06 +0100

    Add gold ui

--- game/src/gold.rs
+++ game/src/gold.rs
@@ -1,11 +1,44 @@
-// Feels a bit unnecessary
+use bevy::color::palettes::css::GOLD;
 
 use crate::prelude::*;
 
 pub(super) fn plugin(app: &mut App) {
     app.insert_resource(Gold(500));
+
+    app.add_systems(OnEnter(AppState::Game), setup_gold_ui);
+
+    app.add_systems(Update, update_gold_ui.run_if(in_state(AppState::Game)));
 }
 
 #[derive(Debug, Default, Resource, Reflect)]
 #[reflect(Resource)]
 pub struct Gold(usize);
+
+#[derive(Debug, Default, Component, Reflect)]
+#[reflect(Component)]
+pub struct GoldText;
+
+fn setup_gold_ui(mut commands: Commands) {
+    commands
+        .spawn((
+            Name::new("Gold UI root"),
+            Node {
+                width: percent(100),
+                height: percent(100),
+                ..Default::default()
+            },
+            DespawnOnExit(AppState::Game),
+        ))
+        .with_children(|parent| {
+            parent.spawn((
+                Name::new("GoldText"),
+                Text::new(""),
+                TextColor(GOLD.into()),
+                GoldText,
+            ));
+        });
+}
+
+fn update_gold_ui(gold: Res<Gold>, mut gold_text: Single<&mut Text, With<GoldText>>) {
+    gold_text.0 = format!("Gold: {}", gold.0);
+}
